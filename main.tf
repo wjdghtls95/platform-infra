b3924ae0642ef0c7e04f17ae7a81edad0e775283
jungho shin <lionjh95@gmail.com>
2025-04-25 01:38:09 +0900

    feat: trigger terraform apply

--- main.tf
+++ main.tf
@@ -15,10 +15,25 @@ resource "aws_security_group" "app_sg" {
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress { from_port = 3000 to_port = 3000 protocol = "tcp" cidr_blocks = ["0.0.0.0/0"] }
-  ingress { from_port = 8000 to_port = 8000 protocol = "tcp" cidr_blocks = ["0.0.0.0/0"] }
+
+  ingress {
+    from_port = 3000
+    to_port = 3000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 8000
+    to_port = 8000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
-    from_port = 0 to_port = 0 protocol = "-1" cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
